63ab58dc0166aa63b13aec0ae2758275302474ce
cj <792965772@qq.com>
2021-08-25 17:48:46 +0800

    .

---
+++ src/main/java/com/cj/mvc/controller/HelloController.java
@@ -0,0 +1,11 @@
+package com.cj.mvc.controller;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * @author CJ
+ * @date 2021/8/25 10:33
+ */
+@Controller
+public class HelloController {
+}
